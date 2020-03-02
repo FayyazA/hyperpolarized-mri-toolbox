645fd0357a0200a62a90669844a26af78192c986
Fayyaz Ahamed <fayyaz_ahamed@berkeley.edu>
2020-03-02 15:02:54 -0800

    Added the kLinflux term and lost confidence intervals

--- kinetic_modeling/fit_pyr_kinetics_and_input_Lacin_Lacex.m
+++ kinetic_modeling/fit_pyr_kinetics_and_input_Lacin_Lacex.m
@@ -55,16 +55,16 @@ end
 
 params_all = {'kPL', 'kLinLex', 'kPA', ...
     'R1P', 'R1L', 'R1A', 'R1Lex', ...
-    'Rinj', 'Tarrival', 'Tbolus','FP','FL'};
+    'Rinj', 'Tarrival', 'Tbolus','FP','FL','kLinflux'};
 params_default_est = [0.01, 0.01, 0.01, ...
     1/30, 1/22, 1/25, 1/15, ...
-    0.1, 0, 8,0.15,0.6];
+    0.1, 0, 8,0.15,0.6,0];
 params_default_lb = [-Inf, -Inf, -Inf, ...
     1/50, 1/38, 1/50, 1/40, ...
-    10^8, 30, 40,1E-08,1E-08];
+    10^8, 30, 40,1E-08,1E-08,0];
 params_default_ub = [Inf, Inf, Inf, ...
     1/10, 1/10, 1/10, 1/35 , ...
-    Inf 60 Inf,10,10];
+    Inf 60 Inf,10,10,0.9];
 
 if nargin < 5 || isempty(params_fixed)
     params_fixed = struct([]);
@@ -406,7 +406,7 @@ u = zeros(1,N);
 
 params_all = {'kPL', 'kLinLex', 'kPA', ...
     'R1P', 'R1L', 'R1A', 'R1Lex', ...
-    'Rinj', 'Tarrival', 'Tbolus','FP','FL'};
+    'Rinj', 'Tarrival', 'Tbolus','FP','FL','kLinflux'};
 
 nfit = 0;
 for n = 1:length(params_all)
@@ -419,8 +419,8 @@ for n = 1:length(params_all)
 end
 
 A = [-R1P-kPL-FP-kPA, 0, 0, 0
-    +kPL, -R1L-kLinLex, 0, 0
-    0, +kLinLex, -R1Lex-FL, 0
+    +kPL, -R1L-kLinLex, kLinflux, 0
+    0, +kLinLex, -R1Lex-FL-kLinflux, 0
     +kPA, 0, 0, -R1A];
 
 % these parameters give a full-width half-max of the bolus of ~ Tbolus sec
